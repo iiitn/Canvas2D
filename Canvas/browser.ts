b6ecf4251c616bd101621227ae56c274f1e6b693
Kishore <kishore.iiitn@gmail.com>
2017-10-29 21:05:19 +0530

    Move browser.ts for easier name resolution.

---
+++ Canvas/browser.ts
@@ -0,0 +1,6 @@
+import {Canvas} from './';
+import {Rectangle} from './Shapes';
+
+// YOUR CODE GOES HERE...
+
+let canvas = new Canvas(document.getElementById("canvas") as HTMLCanvasElement);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	entry: {
-		"index": './browser.ts'
+		"index": './Canvas/browser.ts'
 	},
 	output: {
 		filename: 'bundle/[name].js'
