a318cba0f154e64247c730472901c7f906a9a84e
Kishore <kishore.iiitn@gmail.com>
2017-10-30 16:34:36 +0530

    Add Example code.

--- browser.ts
+++ browser.ts
@@ -4,10 +4,30 @@ import {Rectangle, Circle, Shape} from './Canvas/Shapes';
 // YOUR CODE GOES HERE...
 let canvas = new Canvas(document.getElementById("canvas") as HTMLCanvasElement, true);
 
-let obj: Shape = new Rectangle(10, 10, "green").setPosition(0, 0);
-canvas.addObject(obj);
-obj.moveTo(Math.random()*canvas.width, Math.random()*canvas.height, 1);
 
-obj = new Circle(10, "red").setPosition(0, 0);
-canvas.addObject(obj);
-obj.moveTo(Math.random()*canvas.width, Math.random()*canvas.height, 1);
+let rect: Shape = new Rectangle(10, 10, "green").setPosition(100, 100);
+canvas.addObject(rect);
+
+let circle = new Circle(10, "red").setPosition(0, 0);
+canvas.addObject(circle);
+function move(o: any, speed: number)
+{
+	o.moveTo(Math.random()*canvas.width, Math.random()*canvas.height, speed, ()=>{
+		move(o, speed);
+	});
+}
+move(circle, 1);
+move(rect, 2);
+
+let pause = document.getElementById("pause") as HTMLButtonElement;
+pause.addEventListener("click", ()=>{
+	if (canvas.isPaused) {
+		canvas.resume();
+		pause.innerHTML="Pause";
+	}
+	else {
+		canvas.pause();
+		pause.innerHTML = "Resume";
+	}
+});
+
