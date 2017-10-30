6e61a283abc3327f90128a3e83551c883f1f1a4d
Kishore <kishore.iiitn@gmail.com>
2017-10-30 09:55:35 +0530

    Extend Victor and renamt to Point class.

--- Canvas/Point.ts
+++ Canvas/Point.ts
@@ -1,24 +1,17 @@
-export interface IPoint {
-	x: number
-	y: number
+import Victor = require('victor');
+
+(Victor.prototype as any).interpolate = function(a: Victor, unit: number){
+	let self: Victor = this;
+	let d = self.distance(a)
+	if (d<=(unit+1)) {
+		self.copy(a);
+		return self;
+	}
+	let ix = (unit/d)*(a.x-self.x);
+	let iy = (unit/d)*(a.y-self.y);
+	console.log(ix, iy);
+	self.add(new Victor(ix, iy));
+	return this;
 }
 
-export class Point {
-	/**
-	 * Calculate distance between two points. 
-	 */
-	static getDistance(a: IPoint, b: IPoint): number {
-		return Math.sqrt(Math.pow(b.x-a.x, 2) + Math.pow(b.y-a.y, 2));
-	}
-
-	/**
-	 * Get next interpolated point with `unit` distance.
-	 * @param unit Interpolate distance.
-	 */
-	static getInterpolatedPoint(a: IPoint, b: IPoint, unit: number): IPoint {
-		let d = this.getDistance(a, b);
-		let ix = (unit/d)*(b.x-a.x);
-		let iy = (unit/d)*(b.y-a.y);
-		return {x: ix, y: iy};
-	}
-}
+export class Point extends Victor {}
