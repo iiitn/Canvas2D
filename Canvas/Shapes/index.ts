ff0c7c5b6425e3b6e07e73f0200260f4e7c3aca9
Kishore <kishore.iiitn@gmail.com>
2017-10-30 10:56:07 +0530

    Create Canvas API for gaming and Animation.

---
+++ Canvas/Shapes/index.ts
@@ -0,0 +1,26 @@
+import {Point} from '../Point';
+
+export interface IDimension {
+	x: number
+	y: number
+	width: number
+	height: number
+}
+
+/**
+ * Base Class for all types of shapes that can be
+ * added to Canvas library.
+ */
+export abstract class Shape {
+	protected curPosition: Point;
+	protected _move: {
+		position: Point
+		unit: number
+	};
+
+	abstract render(context: CanvasRenderingContext2D, baseUnit: number): void;
+	abstract moveTo(x: number, y: number, speed?: number): void;
+	abstract getDimensions(): IDimension;
+}
+
+export {Rectangle} from './Rectangle';
--- Canvas/index.ts
+++ Canvas/index.ts
@@ -1,113 +1,74 @@
-let elem = document.getElementById("canvas") as HTMLCanvasElement;
+import {Shape} from './Shapes';
 
-let canvas_width = 500;
-let canvas_height = 400;
-elem.width = canvas_width;
-elem.height = canvas_height;
+export class Canvas {
+	public readonly height: number;
+	public readonly width: number;
 
-let canvas = elem.getContext("2d") as CanvasRenderingContext2D;
+	private baseUnit: number = 10;
+	private context: CanvasRenderingContext2D;
+	private renderIndefinite: boolean;
 
-// Draw grids.
-canvas.beginPath();
-canvas.strokeStyle = "lightgrey";
-canvas.lineWidth = 1;
-canvas.lineCap = 'round';
-canvas.shadowColor = "none";
+	private objects: Shape[] = [];
 
-for (let i=0; i<=canvas_width/100; i++) {
-	canvas.moveTo(i*100, 0);
-	canvas.lineTo(i*100, canvas_height);
-	canvas.stroke();
-}
-for (let i=0; i<=canvas_height/100; i++) {
-	canvas.moveTo(0, i*100);
-	canvas.lineTo(canvas_width, i*100);
-	canvas.stroke();
-}
-canvas.closePath();
-// END DRAWING GRIDS.
+	/**
+	 * Create Canvas.
+	 * @param elem valid DOM canvas element.
+	 * @param renderIndefinite set to false, if fine grained control
+	 * of operations is necessary between Canvas renders. Set
+	 * to true otherwise. Defaults false.
+	 */
+	constructor(elem: HTMLCanvasElement, renderIndefinite = false) {
+		this.width = elem.width?elem.width: 500;
+		this.height = elem.height?elem.height:400;
+		this.renderIndefinite = renderIndefinite;
 
-
-canvas.beginPath();
-canvas.strokeStyle = "green";
-canvas.lineWidth = 2;
-canvas.lineCap = 'round';
-canvas.shadowColor = "green";
-canvas.shadowBlur = 0;
-canvas.shadowOffsetX = 0
-canvas.shadowOffsetY = 0;
-
-type IExecFunction = (done: ()=>void)=>void;
-class Execution {
-	private static list: IExecFunction[] = [];
-	private static done: boolean = true;
-
-	private static callFunc(func: IExecFunction) {
-		console.log("Called Function");
-		this.done = false;
-		func(()=>{
-			this.done = true;
-			this.dequeue();
-		});	
-	}
-	private static dequeue() {
-		console.log("Dequed", this.list);
-		if (this.list.length==0) {
-			return;
-		}
-		let ref = this.list[0];
-		this.list.splice(0, 1);
-		this.callFunc(ref);
-	}
-	static queue(func: (done: ()=>void)=>void) {
-		console.log("Queed", this.list);
-		if (this.done) {
-			this.callFunc(func);
+		let context = elem.getContext("2d");
+		if (context) {
+			this.context = context;
 		}
 		else {
-			this.list.push(func);
+			console.error("Couldn't create context to Canvas Element : ", elem);
+		}
+		this.render = this.render.bind(this);
+
+		// Render if fine control over operations is not necessary.
+		if (this.renderIndefinite) {
+			this.render();
+		}
+	}
+
+	/**
+	 * Adds a shape object to Canvas.
+	 * @param obj valid Shape object.
+	 */
+	addObject(obj: Shape) {
+		this.objects.push(obj);
+	}
+
+	/**
+	 * Remove shape obj from current Canvas.
+	 * @param obj Object to be removed.
+	 */
+	removeObject(obj: Shape) {
+		this.objects = this.objects.filter((o)=>{
+			if (o==obj)
+				return false;
+			return true;
+		});
+	}
+
+	/**
+	 * Renders canvas iterating through all the objects.
+	 */
+	render() {
+		this.context.clearRect(0, 0, this.width, this.height);
+		this.objects.map((shape)=>{
+			shape.render(this.context, this.baseUnit);
+		});
+		this.context.stroke();
+		this.context.fill();
+		if (this.renderIndefinite) {
+			requestAnimationFrame(this.render);
 		}
 	}
 }
-
-
-// Canvas API for users.
-let g_x = 0, g_y = 0;
-export let Canvas = {
-	moveTo(x: number, y: number) {
-		Execution.queue((done)=>{
-			g_x = x;
-			g_y = y;
-			canvas.moveTo(x, y);
-			done();
-		})
-	},
-	lineTo(x: number, y: number) {
-		let move_unit = 10;
-		Execution.queue((done)=>{
-			// Interpolate logic goes here...
-			let d = Math.sqrt(Math.pow(g_x-x, 2) + Math.pow(g_y-y, 2));
-			let interpolateX = (move_unit/d)*(x-g_x);
-			let interpolateY = (move_unit/d)*(y-g_y);
-			let nextX = g_x, nextY = g_y;
-			let interval = setInterval(()=>{
-				if (Math.abs(nextX-x)<=move_unit && Math.abs(nextY-y)<=move_unit) {
-					nextX = x;
-					nextY = y;
-				}
-				else {
-					nextX += interpolateX;
-					nextY += interpolateY;
-				}
-				canvas.lineTo(nextX, nextY);
-				canvas.stroke();
-				if (x==nextX && y==nextY){
-					g_x = x, g_y = y;
-					clearInterval(interval);
-					(interval as any) = null;
-					done();
-				}
-			}, 10);
-		});
-	}
-};
