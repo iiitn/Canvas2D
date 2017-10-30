a8842e58ecc8b4150438fb28ba9c7a7fcab74ba9
Kishore <kishore.iiitn@gmail.com>
2017-10-30 17:01:02 +0530

    Add example.

--- browser.ts
+++ browser.ts
@@ -4,20 +4,26 @@ import {Rectangle, Circle, Shape} from './Canvas/Shapes';
 // YOUR CODE GOES HERE...
 let canvas = new Canvas(document.getElementById("canvas") as HTMLCanvasElement, true);
 
-
-let rect: Shape = new Rectangle(10, 10, "green").setPosition(100, 100);
-canvas.addObject(rect);
-
-let circle = new Circle(10, "red").setPosition(0, 0);
-canvas.addObject(circle);
 function move(o: any, speed: number)
 {
 	o.moveTo(Math.random()*canvas.width, Math.random()*canvas.height, speed, ()=>{
 		move(o, speed);
 	});
 }
-move(circle, 1);
-move(rect, 2);
+let circle: Shape = new Circle(10, "red").setPosition(0, 0);
+move(circle, 0.5);
+
+for (let i=0; i<40; i++) {
+	let rect: Shape = new Rectangle(10, 10, "green").setPosition(0, 0);
+	canvas.addObject(rect);
+	move(rect, 1);
+}
+for (let i=0; i<40; i++) {
+	let circle: Shape = new Circle(5, "green").setPosition(0, 0);
+	canvas.addObject(circle);
+	move(circle, 1);
+}
+canvas.addObject(circle);
 
 let pause = document.getElementById("pause") as HTMLButtonElement;
 pause.addEventListener("click", ()=>{
